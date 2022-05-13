f81014a748ea3f64239937f3f9487ed32ccedc96
Jbolivar13 <103448939+Jbolivar13@users.noreply.github.com>
2022-05-13 16:07:02 -0400

    Delete space-tourism-website-main directory

--- space-tourism-website-main/starter-code/main.js
+++
@@ -1,8 +0,0 @@
-async function obtenerDatos(){
-    const response = await fetch("http://127.0.0.1:5500/starter-code/data.json");
-    const json = await response.json();
-
-    console.log(json.destinations[1]);
-
-}
-obtenerDatos();
